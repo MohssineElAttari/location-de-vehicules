0a2d4024232bf3422b2456939fc3bc98cc0abc47
MohssineElAttari <14mohssine@gmail.com>
2021-02-24 01:35:09 +0100

    new update

--- scripts/script.js
+++ scripts/script.js
@@ -21,25 +21,29 @@ var fCity = document.getElementById("cCity");
 var fMessage = document.getElementById("cMessage");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.querySelector(".close");
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-    // Get the valeu element fNasme
-    document.getElementById("mName").innerHTML = fName.value;
-    document.getElementById("mNumber").innerHTML = fNumber.value;
-    document.getElementById("mEmail").innerHTML = fEmail.value;
-    document.getElementById("mCity").innerHTML = fCity.value;
-    document.getElementById("mMessage").innerHTML = fMessage.value;
-    // mName = fName;
+if (btn) {
+    btn.onclick = function() {
+        // Get the valeu element fNasme
+        document.getElementById("mName").innerHTML = fName.value;
+        document.getElementById("mNumber").innerHTML = fNumber.value;
+        document.getElementById("mEmail").innerHTML = fEmail.value;
+        document.getElementById("mCity").innerHTML = fCity.value;
+        document.getElementById("mMessage").innerHTML = fMessage.value;
+        // mName = fName;
 
 
-    modal.style.display = "block";
+        modal.style.display = "block";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+if (span) {
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -49,73 +53,94 @@ window.onclick = function(event) {
     }
 }
 
+
 // booking
 
-var selected, Fuel = 0,
-    dailyPrice, Percent = 0,
-    duration, total_globale;
 
-selected = document.getElementById("vehicle-type").value;
-console.log(selected);
+var vehicle = document.querySelector("#vehicle-type");
+var energie = document.querySelector("#Fuel-type");
+var boite = document.querySelector("#transmission-type");
+var prix, tax;
 
-function choix_Fuel() {
-    Fuel = document.getElementById("Fuel-type").value;
+function hidden_item(target_element) {
+    target_element.forEach(Element => {
+        Element.style.display = "none";
+    })
 }
 
-function choix_vehicle() { //e=moto
-    switch (selected) {
-
-        case "city-car":
-            document.querySelector("#electric").style.display = "block";
-            document.querySelector("#hybird").style.display = "block";
-            document.querySelector("#gasoline").style.display = "block";
-            document.querySelector("#diesel").style.display = "block";
-            document.querySelector("#manual").style.display = "block";
-            dailyPrice = 12;
-        case "utility-car":
-            document.querySelector("#diesel").style.display = "block";
-            document.querySelector("#manual").style.display = "block";
-            dailyPrice = 16;
-        case "compact-car":
-            document.querySelector("#electric").style.display = "block";
-            document.querySelector("#hybird").style.display = "block";
-            document.querySelector("#gasoline").style.display = "block";
-            document.querySelector("#diesel").style.display = "block";
-            document.querySelector("#manual").style.display = "block";
-            dailyPrice = 12;
-        case "sedan-car":
-            document.querySelector("#hybird").style.display = "block";
-            document.querySelector("#gasoline").style.display = "block";
-            document.querySelector("#diesel").style.display = "block";
-            document.querySelector("#automatic").style.display = "block";
-            dailyPrice = 20;
-            Percent = 0.19;
-        case "motocycle":
-            document.querySelector("#gasoline").style.display = "block";
-            document.querySelector("#electric").style.display = "block";
-            dailyPrice = 10;
+function vehicle_fun() {
+    switch (vehicle.value) {
+        case "City Car":
+            prix = 12;
+            document.querySelectorAll(".fuel").forEach((Element) => {
+                Element.style.display = "block";
+            })
+            hidden_item(document.querySelectorAll(".boite"));
+            document.querySelector('#manual').style.display = "block";
             break;
-
-        case "heavy-equipment":
-            document.querySelector("#gasoline").style.display = "block";
-            document.querySelector("#diesel").style.display = "block";
-            document.querySelector("#manual").style.display = "block";
-            dailyPrice = 900;
-        case "truck":
-            document.querySelector("#diesel").style.display = "block";
-            document.querySelector("#automatic").style.display = "block";
-            dailyPrice = 250;
-            Percent = 0.19;
+        case "Utility Vehicle":
+            hidden_item(document.querySelectorAll(".fuel"));
+            prix = 16;
+            document.querySelector(".diesel").style.display = "block";
+            hidden_item(document.querySelectorAll(".boite"));
+            document.querySelector('#manual').style.display = "block";
+            break;
+        case "Compact Car":
+            hidden_item(document.querySelectorAll(".fuel"));
+            prix = 14;
+            document.querySelector(".diesel").style.display = "block";
+            document.querySelector(".gasoline").style.display = "block";
+            document.querySelector(".hybrid").style.display = "block";
+            hidden_item(document.querySelectorAll(".boite"));
+            document.querySelector('#manual').style.display = "block";
+            break;
+        case "Sedan Car":
+            hidden_item(document.querySelectorAll(".fuel"));
+            prix = 20;
+            document.querySelector(".diesel").style.display = "block";
+            document.querySelector(".gasoline").style.display = "block";
+            document.querySelector(".hybrid").style.display = "block";
+            hidden_item(document.querySelectorAll(".boite"));
+            document.querySelector('#automatic').style.display = "block";
+            break;
+        case "Motorcycle":
+            hidden_item(document.querySelectorAll(".fuel"));
+            prix = 10;
+            document.querySelector(".gasoline").style.display = "block";
+            document.querySelector(".electric").style.display = "block";
+            hidden_item(document.querySelectorAll(".boite"));
+            break;
+        case "Heavy Equipment":
+            hidden_item(document.querySelectorAll(".fuel"));
+            prix = 900;
+            document.querySelector(".gasoline").style.display = "block";
+            document.querySelector(".diesel").style.display = "block";
+            hidden_item(document.querySelectorAll(".boite"));
+            document.querySelector('#manual').style.display = "block";
+            break;
+        case "Truck":
+            hidden_item(document.querySelectorAll(".fuel"));
+            prix = 250;
+            document.querySelector(".diesel").style.display = "block";
+            hidden_item(document.querySelectorAll(".boite"));
+            document.querySelector('#automatic').style.display = "block";
+            break;
     }
 }
 
-function price() {
-    var number_Days = document.getElementById("numberDays").value;
-
-    // Price of booking calculation
-
-    total_globale = (dailyPrice + (dailyPrice * (Fuel + Percent))) * number_Days;
-
-    getElementById('priceis').innerHTML = total;
-
+function choix_Fuel() {
+    tax = Number(energie.value);
 }
+
+function boite_choix() {
+    if (boite.value == "automatic") {
+        tax += 0.19;
+    }
+}
+var days,
+    total;
+document.querySelector(".btn_calc").addEventListener("click", function() {
+    days = Number(document.getElementById("numberDays").value);
+    total = Math.round(days * (prix + (prix * tax)));
+    window.alert(`Days : ${days}\nPrice : €${prix}\nTax : ${Math.round(tax*100)}%\nTotal : €${total}`)
+})
